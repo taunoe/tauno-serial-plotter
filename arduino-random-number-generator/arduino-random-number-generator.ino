95c468121063ebf02d1628606de36b10b6e94afd
Tauno Erik <sedumacre@gmail.com>
2021-02-20 21:31:14 +0200

    new branch

--- arduino-random-number-generator/arduino-random-number-generator.ino
+++ arduino-random-number-generator/arduino-random-number-generator.ino
@@ -1,4 +1,6 @@
-
+/*
+ * Copyright 20.02.2021 Tauno Erik
+ */
 
 
 void setup() {
@@ -8,37 +10,39 @@ void setup() {
 }
 
 void loop() {
-  // print a random number from 0 to 299
+  // print a random numbers
   long r1 = random(10);
   Serial.print(r1);
   Serial.print("_");
 
   long r2 = random(10, 20);
-  Serial.print(r2);
-  Serial.print("_");
+  Serial.print(r2*1.2);
+  Serial.print(",");
 
   long r3 = random(20, 30);
   Serial.print(r3);
-  Serial.print("_");
+  Serial.print(".");
 
   long r4 = random(30, 40);
   Serial.print(r4);
-  Serial.print("_");
+  Serial.print("a");
 
   long r5 = random(40, 50);
   Serial.print(r5);
-  Serial.print("_");
+  Serial.print(":");
 
   long r6 = random(50, 60);
   Serial.print(r6);
-  Serial.print("_");
+  Serial.print(";");
 
   long r7 = random(60, 70);
   Serial.print(r7);
-  Serial.print("_");
+  Serial.print("!");
 
   long r8 = random(70, 80);
-  Serial.println(r8);
+  Serial.print(r8/1.5);
+  
+  Serial.println("Min:0, Max:100");
 
   delay(100);
 }
